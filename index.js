9fcfe4c0c1fe9293dfeebca20554f206732bb399
Pedro R <rochapedro436@gmail.com>
2023-05-24 22:54:58 -0300

    Otimização código

--- index.js
+++ index.js
@@ -21,19 +21,17 @@ app.post('/api/client', async (req, res) => {
         "text": mensagem
       };
      
-    urlWpp = process.env.APIWhatssApp
-    response = await axios.post(urlWpp, body)
-    
+    response = await axios.post( process.env.APIWhatssApp, body)
+    console.log(response.data)
     if (!name | !number ) {
       console.log("Faltando alguma variável")
-      console.log(response.data)
-      return res.status(400).json({ error: 'Nome, número e mensagem são obrigatórios' });
+      return res.status(400).json({ error: 'Nome e número  são obrigatórios' });
     }
     else{
       console.log("Sucesso!")
-      console.log(response.data)
       return res.status(200).json({mensagem:"solicitação enviada com sucesso", name: name})
     }
+    
   } catch (error) {
     console.error('Erro na chamada da segunda API:', error);
     res.status(500).json({ error: 'Erro na chamada da segunda API' });

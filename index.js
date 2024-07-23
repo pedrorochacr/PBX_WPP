2f00d430500a9060af8633a3f76398f9a82e401d
Pedro R <rochapedro436@gmail.com>
2024-07-22 23:25:02 -0300

    update port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 8080;
 require('dotenv').config();
 app.use(express.json());
 app.post('/api/client', async (req, res) => {      

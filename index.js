ef6765821bb6f102cacf152e3fe63e0744b447ec
Pedro R <rochapedro436@gmail.com>
2024-07-22 23:15:02 -0300

    some updates

--- index.js
+++ index.js
@@ -3,26 +3,24 @@ const axios = require('axios');
 
 const app = express();
 const port = 3000;
-
+require('dotenv').config();
 app.use(express.json());
 app.post('/api/client', async (req, res) => {      
   try {
     const { name, number,mensagem, token } = req.body;
-   
-    
+       
     console.log(name)
     console.log(number)
-
 
     const body = {
         "type": "0",
         "token": token,
         "numero": "55"+number,
         "text": mensagem
-      };
-     
-    response = await axios.post( process.env.APIWhatssApp, body)
-    console.log(response.data)
+    };
+    console.log(process.env.APIWhatssApp)
+    response = await axios.post(process.env.APIWhatssApp, body)
+    console.log("response",response)
     if (!name | !number ) {
       console.log("Faltando alguma variável")
       return res.status(400).json({ error: 'Nome e número  são obrigatórios' });
@@ -34,7 +32,7 @@ app.post('/api/client', async (req, res) => {
     
   } catch (error) {
     console.error('Erro na chamada da segunda API:', error);
-    res.status(500).json({ error: 'Erro na chamada da segunda API' });
+    res.status(500).json({ error: error });
   }
 });
 
